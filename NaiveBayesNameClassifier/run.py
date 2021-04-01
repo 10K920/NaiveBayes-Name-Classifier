6e7f6871b709427af3c461f57aad52c44b232683
10K920 <80634597+10K920@users.noreply.github.com>
2021-04-01 05:40:41 -0500

    Add files via upload

---
+++ NaiveBayesNameClassifier/run.py
@@ -0,0 +1,10 @@
+import numpy as np
+from classifyLinear import classifyLinear
+from name2features import name2features
+from genTrainFeatures import genTrainFeatures
+from naivebayesCL import naivebayesCL
+from example_tests import example_tests
+
+#xTr,yTr = genTrainFeatures()
+#w,b = naivebayesCL(xTr,yTr)
+example_tests()
